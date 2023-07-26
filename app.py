f89df97866d850f7f8b046e1dc2f9e246f75d7fe
roua <abdelbariroua85@gmail.com>
2023-07-26 16:19:53 +0100

    App flask

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import json
-from flask import Flask, render_template, request
+import jwt
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = 'ma_cle_secrete'
 
 registered_users = []
 
@@ -23,20 +25,25 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        if not is_valid_email(email):
-            return "Veuillez entrer une adresse email valide."
-
-        if not is_valid_password(password):
-            return "Le mot de passe doit contenir au moins 6 caractères."
-
-        return "Connexion réussie !"
+        # Vérifier les informations d'authentification ici (à compléter selon vos besoins)
+        if email == 'votre_email_valide' and password == 'votre_mot_de_passe_valide':
+            # Créer un JWT avec l'e-mail de l'utilisateur
+            payload = {'email': email}
+            token = jwt.encode(payload, app.secret_key, algorithm='HS256')
+            
+            # Enregistrez le JWT dans la session de l'utilisateur
+            session['user_token'] = token
+            
+            return redirect(url_for('show_users'))
+        else:
+            return "Identifiants invalides. Veuillez réessayer."
 
 def read_data_from_json():
     try:
         with open('data.json', 'r') as f:
             return json.load(f)
     except FileNotFoundError:
-        return [] 
+        return []
 
 def write_data_to_json(data):
     with open('data.json', 'w') as f:
@@ -59,11 +66,33 @@ def register():
         registered_users.append({'email': email, 'password': password})
         write_data_to_json(registered_users)
         return "Inscription réussie !"
-    
+
+def get_user_data(email):
+    for user in registered_users:
+        if user['email'] == email:
+            return user
+    return None
+
 @app.route('/users')
 def show_users():
-    registered_users = read_data_from_json()
-    return render_template('users.html', users=registered_users)
+    # Vérifier si l'utilisateur est authentifié en vérifiant la présence du JWT dans la session
+    if 'user_token' in session:
+        try:
+            # Décoder le JWT pour obtenir l'e-mail de l'utilisateur
+            token = session['user_token']
+            payload = jwt.decode(token, app.secret_key, algorithms=['HS256'])
+            email = payload['email']
+
+            # Utilisez l'e-mail pour obtenir les informations de l'utilisateur à afficher
+            user_data = get_user_data(email)
+
+            return render_template('users.html', users=[user_data])
+        except jwt.ExpiredSignatureError:
+            return "Session expirée. Veuillez vous connecter à nouveau."
+        except jwt.InvalidTokenError:
+            return "Session invalide. Veuillez vous connecter à nouveau."
+    else:
+        return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
